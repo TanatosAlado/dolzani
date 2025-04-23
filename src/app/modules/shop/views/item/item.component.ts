3f221b904a30ba4deeed2c8f5454a09172b508a0
Cristian Alessandria <cristian_alessandria@hotmail.com>
2025-04-23 07:55:53 -0300

    Destacados

--- src/app/modules/shop/views/item/item.component.ts
+++ src/app/modules/shop/views/item/item.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.css']
+  styleUrls: ['./item.component.css'],
 })
 export class ItemComponent {
-
+  @Input() producto!: {
+    nombre: string;
+    descripcion?: string;
+    imagenUrl: string;
+    precioOriginal: number;
+    precioConDescuento: number;
+    descuento: number;
+  };
 }
