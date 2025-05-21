4a2fa93f2f8402bc9c22ffad53a20cd106f71fff
Tanatos <cristian_alessandria@hotmail.com>
2025-05-21 00:36:22 -0300

    fix vista

--- src/app/modules/shop/views/item/item.component.ts
+++ src/app/modules/shop/views/item/item.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Cliente } from 'src/app/modules/auth/models/cliente.model';
+import { IngresoComponent } from 'src/app/modules/auth/views/ingreso/ingreso.component';
 import { Producto } from 'src/app/shared/models/producto.model';
 import { GeneralService } from 'src/app/shared/services/general.service';
 import { ToastService } from 'src/app/shared/services/toast.service';
@@ -11,21 +14,65 @@ import { ToastService } from 'src/app/shared/services/toast.service';
 export class ItemComponent {
   @Input() producto!: Producto;
 
-  constructor(private generalService:GeneralService, private toastService: ToastService){
+  constructor(private generalService:GeneralService, private toastService: ToastService, private dialog: MatDialog){
 
   }
 
  
   //FUNCION PARA AGREGAR EL CARRITO EN EL CLIENTE
-  agregarCarrito(id: any) {
-    this.generalService.cargarProductoCarrito(id, 1)
-      .then(() => {
-        this.toastService.toatsMessage('Producto agregado al carrito', 'green',2000);
-      })
-      .catch(err => {
-        this.toastService.toatsMessage('El producto no pudo agregarse', 'red',2000);
-      });
+  // agregarCarrito(id: any) {
+  //   this.generalService.cargarProductoCarrito(id, 1)
+  //     .then(() => {
+  //       this.toastService.toatsMessage('Producto agregado al carrito', 'green',2000);
+  //     })
+  //     .catch(err => {
+  //       this.toastService.toatsMessage('El producto no pudo agregarse', 'red',2000);
+  //     });
+  // }
+
+agregarCarrito(producto: any) {
+  const cliente = this.generalService.getClienteActual();
+
+  if (!cliente) {
+    const dialogRef = this.dialog.open(IngresoComponent, {
+      width: '400px',
+      disableClose: true,
+      backdropClass: 'custom-backdrop',
+      panelClass: 'custom-dialog'
+    });
+
+    dialogRef.afterClosed().subscribe((clienteLogueado: Cliente) => {
+      if (clienteLogueado) {
+        this.generalService.setCliente(clienteLogueado);
+        localStorage.setItem('cliente', clienteLogueado.id);
+        
+        this.generalService.cargarProductoCarrito(producto, 1)
+          .then(() => {
+            this.toastService.toatsMessage('Producto agregado al carrito', 'green', 2000);
+          })
+          .catch(err => {
+            this.toastService.toatsMessage('El producto no pudo agregarse', 'red', 2000);
+            console.error(err);
+          });
+      } else {
+        this.toastService.toatsMessage('Debe iniciar sesión para agregar productos al carrito', 'orange', 3000);
+      }
+    });
+
+    return;
   }
+
+  // Usuario ya logueado
+  this.generalService.cargarProductoCarrito(producto, 1)
+    .then(() => {
+      this.toastService.toatsMessage('Producto agregado al carrito', 'green', 2000);
+    })
+    .catch(err => {
+      this.toastService.toatsMessage('El producto no pudo agregarse', 'red', 2000);
+      console.error(err);
+    });
+}
+
 
   calcularDescuento(producto: Producto): number {
   if (producto.oferta && producto.precio > 0 && producto.precioOferta > 0) {
