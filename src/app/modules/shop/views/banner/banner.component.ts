ba42d0ddb2ca6a75de9640dadd836436a159e47a
Tanatos <cristian_alessandria@hotmail.com>
2025-05-22 21:46:50 -0300

    Fix banner

--- src/app/modules/shop/views/banner/banner.component.ts
+++ src/app/modules/shop/views/banner/banner.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { BannerService } from 'src/app/modules/admin/service/banner.service';
 
+declare var bootstrap: any;
+
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
@@ -10,16 +12,28 @@ export class BannerComponent {
 
   imagenes: { nombre: string, url: string }[] = [];
 
-  constructor(private bannerService: BannerService) {}
+
+  constructor(private bannerService: BannerService) { }
 
   ngOnInit(): void {
     this.cargarImagenes();
   }
 
-
   async cargarImagenes() {
-    // Asegurate que coincida con la carpeta usada en el upload
     this.imagenes = await this.bannerService.listarArchivos('uploads');
+
+    // Esperamos un "tick" para que Angular pinte el DOM actualizado
+    setTimeout(() => {
+      const el = document.querySelector('#bannerCarousel');
+      if (el) {
+        const carousel = bootstrap.Carousel.getOrCreateInstance(el, {
+          interval: 3000,
+          ride: 'carousel',
+          pause: false
+        });
+        carousel.cycle();
+      }
+    });
   }
 
 
